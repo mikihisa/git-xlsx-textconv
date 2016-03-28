6dc82ec8cb9a7a49f5a2c25cbd53ee9430cf411d
Motoki IDO <mikihisa@creativefreaks.net>
2016-03-28 13:39:00 +0900

    refix.

--- git-xlsx-textconv.go
+++ git-xlsx-textconv.go
@@ -28,7 +28,9 @@ func main() {
 			cels := make([]string, len(row.Cells))
 			for i, cell := range row.Cells {
 				s,err := cell.String()
-				err = ""
+				if err != nil {
+					fmt.Printf("Error [%s]\n", err)
+				}
 				s = strings.Replace(s, "\\", "\\\\", -1)
 				s = strings.Replace(s, "\n", "\\n", -1)
 				s = strings.Replace(s, "\r", "\\r", -1)
